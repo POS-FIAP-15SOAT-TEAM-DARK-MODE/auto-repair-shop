83c1f12e0c0a22a9dd450082cb36e51ceb365151
Caetano Freitas <caetanofreitas72@gmail.com>
2026-03-07 20:59:23 -0300

    feat: add logger

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/POS-FIAP-15SOAT-TEAM-DARK-MODE/auto-repair-shop/internal/infra/factory"
+	"github.com/POS-FIAP-15SOAT-TEAM-DARK-MODE/auto-repair-shop/internal/pkg/logger"
 	"github.com/POS-FIAP-15SOAT-TEAM-DARK-MODE/auto-repair-shop/internal/routing"
 )
 
 func main() {
+	defer logger.Sync()
+
 	container := factory.HttpContainer()
 	router := routing.SetupRouter(container)
 	router.Run(":8080")
--- internal/infra/handler/ping/handler.go
+++ internal/infra/handler/ping/handler.go
@@ -4,8 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 
 	"github.com/POS-FIAP-15SOAT-TEAM-DARK-MODE/auto-repair-shop/internal/domain"
+	"github.com/POS-FIAP-15SOAT-TEAM-DARK-MODE/auto-repair-shop/internal/pkg/logger"
 )
 
 type Handler struct {
@@ -18,5 +20,6 @@ func NewHandler(uc domain.PingService) *Handler {
 
 func (h *Handler) Ping(c *gin.Context) {
 	result := h.svc.Execute()
+	logger.Info("ping called", zap.String("response", result.Message))
 	c.JSON(http.StatusOK, gin.H{"message": result.Message})
 }
