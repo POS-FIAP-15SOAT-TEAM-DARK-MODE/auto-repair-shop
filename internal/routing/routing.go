634ca2a0f0c75ab709d4ca1ab9ed09cc78841d5e
Caetano Freitas <caetanofreitas72@gmail.com>
2026-03-07 20:59:41 -0300

    feat: add panic recovery

--- internal/routing/routing.go
+++ internal/routing/routing.go
@@ -4,10 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/POS-FIAP-15SOAT-TEAM-DARK-MODE/auto-repair-shop/internal/infra/container"
+	"github.com/POS-FIAP-15SOAT-TEAM-DARK-MODE/auto-repair-shop/internal/infra/middleware"
 )
 
 func SetupRouter(c *container.HTTP) *gin.Engine {
-	router := gin.Default()
+	router := gin.New()
+
+	router.Use(middleware.Logger())
+	router.Use(middleware.Recovery())
+	router.Use(middleware.ErrorHandler())
 
 	router.GET("/ping", c.PingHandler.Ping)
 
