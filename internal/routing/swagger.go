156bf87b08141827367904c4bcca01dedf4d96da
Giusier F. <giusier@hotmail.com>
2026-03-09 23:04:48 -0300

    chore: update swagger mounting

--- internal/routing/swagger.go
+++ internal/routing/swagger.go
@@ -1,12 +1,19 @@
 package routing
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+
+	"github.com/POS-FIAP-15SOAT-TEAM-DARK-MODE/auto-repair-shop/api"
 )
 
 func mountSwaggerUI(router *gin.Engine) {
-	router.StaticFile("/swagger.yaml", "./api/swagger.yaml")
+	router.GET("/swagger.yaml", func(c *gin.Context) {
+		c.Data(http.StatusOK, "application/yaml; charset=utf-8", api.SwaggerBytes)
+	})
+
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.URL("/swagger.yaml")))
 }
