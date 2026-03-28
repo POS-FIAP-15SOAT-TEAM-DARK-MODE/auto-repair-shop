d574580d29c722c4fb83937cb0f0e7ccdceb7fb0
Giusier F. <giusier@hotmail.com>
2026-03-28 10:25:15 -0300

    feat: add public route validation

--- internal/infra/middleware/auth.go
+++ internal/infra/middleware/auth.go
@@ -1,13 +1,31 @@
 package middleware
 
 import (
-	"github.com/POS-FIAP-15SOAT-TEAM-DARK-MODE/auto-repair-shop/internal/pkg/logger"
 	"github.com/gin-gonic/gin"
 )
 
+// TODO: Remove loose string and convert them into constants
+
+var publicRoutes = map[string]struct{}{
+	"/v1/auth/login":    {},
+	"/v1/auth/register": {},
+	"/ping":             {},
+}
+
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		logger.Of(c.Request.Context()).Info("Auth middleware")
-		c.Next()
+		if isPublicRoute(c.Request.URL.Path) {
+			c.Next()
+			return
+		}
 	}
+}
+
+func isPublicRoute(path string) bool {
+	if len(path) >= 8 && path[:8] == "/swagger" {
+		return true
+	}
+
+	_, ok := publicRoutes[path]
+	return ok
 }
