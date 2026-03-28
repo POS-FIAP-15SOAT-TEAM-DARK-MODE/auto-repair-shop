41af8c7fb205c34f6a1384819fb030254e951d89
Giusier F. <giusier@hotmail.com>
2026-03-28 18:11:29 -0300

    chore: review email and password

--- migrations/000002_create_user_admin.up.sql
+++ migrations/000002_create_user_admin.up.sql
@@ -1,6 +1,6 @@
 -- Create the first user admin of the application
 INSERT INTO "user" (id, name, email, password_hash)
-VALUES ('3b123072-5ad5-4df8-8d3b-3c2770f11b2a', 'admin', 'admin@email.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
+VALUES ('3b123072-5ad5-4df8-8d3b-3c2770f11b2a', 'admin', 'admin@example.com', '$2a$12$kB/dmOdNjFzz78SeWxH3Lud7XJ5Q3UEuE32ahQ6ldbHp3Ztg5L3y.');
 
 -- Insert role to this user
 INSERT INTO "user_role" (id, user_id, role_id)
