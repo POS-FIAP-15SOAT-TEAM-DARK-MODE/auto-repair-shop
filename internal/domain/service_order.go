ee7cb35d112579571ca63bf1305fd4b03ea890fc
Giusier F. <giusier@hotmail.com>
2026-04-05 13:01:05 -0300

    feat: add a new struct for the so service

--- internal/domain/service_order.go
+++ internal/domain/service_order.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"context"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/shopspring/decimal"
@@ -17,6 +18,8 @@ func (s SERVICE_ORDER_STATUS) String() string {
 	return string(s)
 }
 
+type (
+	ServiceOrder struct {
 type ServiceOrder struct {
 	ID          string
 	Status      SERVICE_ORDER_STATUS
@@ -26,6 +29,14 @@ type ServiceOrder struct {
 	Supplies    []Supply
 	TotalAmount decimal.Decimal
 }
+
+	ServiceOrderHistory struct {
+		ID             string
+		PreviousStatus SERVICE_ORDER_STATUS
+		NewStatus      SERVICE_ORDER_STATUS
+		CreatedAt      time.Time
+	}
+)
 
 //go:generate go run github.com/vektra/mockery/v2@latest --name=ServiceOrderService --with-expecter
 //go:generate go run github.com/vektra/mockery/v2@latest --name=ServiceOrderRepository --with-expecter
