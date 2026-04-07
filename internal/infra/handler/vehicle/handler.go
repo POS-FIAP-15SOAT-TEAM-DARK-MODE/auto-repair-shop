8f6c1167a743622b98903033f75c3902bbbad045
Guilherme <gfsantos111@gmail.com>
2026-04-07 00:08:18 -0300

    chore: units tests for handler

--- internal/infra/handler/vehicle/handler.go
+++ internal/infra/handler/vehicle/handler.go
@@ -142,8 +142,18 @@ func (h *handler) Delete(c *gin.Context) {
 }
 
 func (h *handler) FindByCustomer(c *gin.Context) {
-	params, err := createListParams(c)
 	ctx := c.Request.Context()
+	params, err := createListParams(c)
+	if err != nil {
+		status, resp := web.Error(err)
+		logger.Of(ctx).Debug("get vehicles error",
+			zap.String("operation", "get_by_customer_id"),
+			zap.Error(err),
+			zap.String("entity", "vehicle"),
+		)
+		c.JSON(status, resp)
+		return
+	}
 
 	listVehicles, err := h.service.List(ctx, params)
 	if err != nil {
